6ec40d0e44088d1c25f8ec628110e2fd1810a38f
jieun419 <crg1050@naver.com>
2023-08-25 22:07:33 +0900

    chore: a태그 Link태그로 변경 - Link태그로만 변경해주면 SPA가 됨

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import './globals.css'
 import type { Metadata } from 'next'
 
@@ -14,15 +15,15 @@ export default function RootLayout({
   return (
     <html>
       <body>
-        <h1><a href="/">WEB</a></h1>
+        <h1><Link href="/">WEB</Link></h1>
         <ol>
-          <li><a href="/read/1">html</a></li>
-          <li><a href="/read/2">css</a></li>
+          <li><Link href="/read/1">html</Link></li>
+          <li><Link href="/read/2">css</Link></li>
         </ol>
         {children}
         <ul>
-          <li><a href="/create">create</a></li>
-          <li><a href="/update/id">update</a></li>
+          <li><Link href="/create">create</Link></li>
+          <li><Link href="/update/id">update</Link></li>
           <li><button>delete</button></li>
         </ul>
       </body>
